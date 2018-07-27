90ba2973cccf317529e5666b19f2c2c91723702a
XSS <2530468270@qq.com>
2018-07-27 15:34:27 +0800

    Web code

---
+++ js/main_page.js
@@ -0,0 +1,33 @@
+function jumpClick(){
+    var textvalue = $("input[class='form-control']").val();
+    if(textvalue.length!=''){
+        $("#answer  tr:not(:first)").html("");
+       $.ajax({
+            async: true,                                              
+            url: "http://bigcode.fudan.edu.cn/kg/sentenceSearch/",    
+            type: "post",                                             
+            contentType: "application/json; charset=utf-8",           
+            data: JSON.stringify({"os_question":textvalue}),              
+            error: function (xhr, status, errorThrown) {              
+                console.log("Error " + errorThrown);
+                console.log("Status: " + status);
+                console.log(xhr)
+            },
+            success: function(d){
+                sentences = d["sentence_list"]
+                if(sentences.length == 0){
+                    alter("warning","Can't find related sentence!")
+                }    
+                sentences.forEach(function(line,index,sentences){
+                    s = "<tr><td>" + line.doc_id + "</td><td>" + line.text + "</td><td>" ;
+                    $("#answer").append(s);
+                    $("#answer").style.display='block';
+                })  
+            }                            
+        });
+    }else{
+        alert("输入为空！");
+    }
+    
+}
+
